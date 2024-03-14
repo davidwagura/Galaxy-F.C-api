1d9c24ba13a32cc215dafcff47a8d3a3a3227f05
David wanjohi <davidwagura83@gmail.com>
2024-03-14 07:52:16 +0300

    edit table

--- app/Http/Controllers/PlayersController.php
+++ app/Http/Controllers/PlayersController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Player;
 use Illuminate\Http\Request;
 
 class PlayersController extends Controller
 {
-    //
+    public function index()
+    {
+        $players = Player::all();
+        return response()->json($players);
+    }
+
+    public function create()
+    {
+        return response()->json();
+    }
+
+    public function store(Request $request)
+    {   
+        $player = new Player;
+        $player->name = $request->name;
+        $player->age = $request->age;
+        $player->nationality = $request->nationality;
+        $player->position = $request->position;
+        $player->save();
+
+        return response()->json($player);
+    }
+
+    public function show(string $id)
+    {
+        $player = Player:: findorfail($id);
+        return response()->json($player);
+    }
+
+    public function edit(string $id)
+    {
+        $player = Player::findorfail($id);
+        return response()->json($player);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $player = Player::findorfail($id);
+        $player->name = $request->name;
+        $player->age = $request->age;
+        $player->nationality = $request->nationality;
+        $player->position = $request->position;
+        $player->save();
+
+        return response()->json($player);
+    }
+
+    public function destroy(string $id)
+    {
+        $player = Player::findorfail($id);
+        $player->dalete();
+        return response()->json($player);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\PlayersController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// Route::resource('players', PlayersController::class);
